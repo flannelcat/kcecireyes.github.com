2581d72f7098c80436e066b8bc484e9676cbc49c
kcecireyes <kcecireyes@gmail.com>
2015-02-03 09:44:52 -0500

    optimized some things for mobile

--- js/data.js
+++ js/data.js
@@ -2,137 +2,130 @@ data =
 [{
 	"organization": "Boston Globe",
 	"class": "bg",
+	"title": "MFA Masterpieces on Loan",
+	"link":"http://www.bostonglobe.com/2014/08/02/mfa-masterpieces-loan/nKWiWAouJFiHC8GHOG39UI/story.html",
+	"image": "images/work_samples/globe/loanart.png"
+},{
+	"organization": "Boston Globe",
+	"class": "bg",
+	"title": "NFL Pick 'Em",
+	"link":"http://www.bostonglobe.com/2014/09/02/globe-nfl-pick/lrrDKN5JOcILOf4NAc95kN/story.html",
+	"image": "images/work_samples/globe/pickem.png"
+},{
+	"organization": "Boston Globe",
+	"class": "bg",
 	"title": "Ten Percenters",
 	"link":"http://www.bostonglobe.com/metro/2014/07/19/just-luck-some-lottery-winners-cash-hundreds-tickets/u9slYhTdzofrnTGMmGF0jN/story.html",
 	"image": "images/work_samples/globe/brudnicks.png"
-},
-{
+},{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "Mass. by Descendance",
 	"link":"http://www.bostonglobe.com/2014/07/14/what-team-did-massachusetts-root-for-during-world-cup-final/EypFKrpWFpLV4l7kvnvu8O/story.html",
 	"image": "images/work_samples/globe/worldcup.png"
-},
-{
+},{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "FIFA 2014: Will the U.S. advance if...",
 	"link":"http://www.bostonglobe.com/2014/06/23/soccer/pf21frmdANwSDtGvBVhWVK/story.html",
 	"image": "images/work_samples/globe/wc_calculator.png"
-},
-{
+},{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "Rising Tuition Costs in Boston Area",
 	"link":"http://www.bostonglobe.com/2014/06/22/compare-prices-colleges-and-universities/383o9UzIKNeHHm5yODNuQO/story.html",
 	"image": "images/work_samples/globe/colleges.png"
-},
-{
+},{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "Boston Mayoral Race",
 	"link":"http://www.bostonglobe.com/2014/06/05/poll-governor-election-candidates/rnLiEf8tFWjjaPFxXRvwRK/story.html",
 	"image": "images/work_samples/globe/favorability.png"
-},
-{
+},{
 	"organization": "New York Daily News",
 	"class": "nydn",
 	"title": "GEN Hackathon 2014",
 	"link":"https://github.com/kcecireyes/NewsQs",
 	"image": "images/work_samples/nydn/newsqs.png"
-},
-{
+},{
 	"organization": "New York Daily News",
 	"class": "nydn",
 	"title": "What Does Autocomplete Say About...",
 	"link":"http://www.nydailynews.com/new-york/map-reveals-google-autocomplete-new-york-city-article-1.1744115",
 	"image": "images/work_samples/nydn/google.png"
-},
-{
+},{
 	"organization": "New York Daily News",
 	"class": "nydn",
 	"title": "Sandy Anniversary",
 	"link":"http://www.nydailynews.com/new-york/hurricane-sandy/sandy-flood-zone-interactive-graphic-article-1.1499707",
 	"image": "images/work_samples/nydn/sandy.png"
-},
-{
+},{
 	"organization": "Columbia Spectator",
 	"class": "spec",
 	"title": "Faculty House Negotiations Stalled",
 	"link":"http://columbiaspectator.com/2013/01/16/students-faculty-house-employees-continue-negotiate-contracts-labor-administrators",
 	"image": "images/work_samples/spec/sws.jpg"
-},
-{
+},{
 	"organization": "Columbia Spectator",
 	"class": "spec",
 	"title": "TC Administrators Pocket $315,500 from Budget",
 	"link":"http://columbiaspectator.com/news/2013/05/10/teachers-college-faculty-vote-not-support-proposed-2013-14-budget",
 	"image": "images/work_samples/spec/tc.jpg"
-},
-{
+},{
 	"organization": "Columbia Spectator",
 	"class": "spec",
 	"title": "Columbia Nutella Craze",
 	"link":"http://columbiaspectator.com/2013/03/05/nutella-ferris-booth-costs-dining-5000-week-part-due-dining-hall-thievery",
 	"image": "images/work_samples/spec/nutella.jpg"
-},
-{
+},{
 	"organization": "Columbia Spectator",
 	"class": "spec",
 	"title": "Police Sting at Bar 1020",
 	"link":"http://columbiaspectator.com/2013/02/22/1020-fined-underage-drinking-after-police-sting-could-lose-liquor-license",
 	"image": "images/work_samples/spec/1020.jpg"
-},
-{
+},{
 	"organization": "Projects",
 	"class":"projects",
 	"title": "Jit: A Language for Journalists",
 	"link":"https://github.com/kcecireyes/JIT",
 	"image": "images/work_samples/drawings/jit.png"
-},
-{
+},{
 	"organization": "Projects",
 	"class":"projects",
 	"title": "Get With The Times - HackNY 2012",
 	"link":"https://github.com/alexandraorth/GetWithTheTimes",
 	"image": "images/work_samples/drawings/gwtt.png"
-},
-{
+},{
 	"organization": "Drawings",
 	"class":"drawings",
 	"title": "Study of Legs",
 	"link":"images/work_samples/drawings/1.jpg",
 	"image": "images/work_samples/drawings/1.jpg"
-},
-{
+},{
 	"organization": "Drawings",
 	"class":"drawings",
 	"title": "Study 2",
 	"link":"images/work_samples/drawings/3.jpg",
 	"image": "images/work_samples/drawings/3.jpg"
-},
-{
+},{
 	"organization": "Drawings",
 	"class":"drawings",
 	"title": "Portrait of Sleeping Girl",
 	"link":"images/work_samples/drawings/5.jpg",
 	"image": "images/work_samples/drawings/5.jpg"
-},
-{
+},{
 	"organization": "Drawings",
 	"class":"drawings",
 	"title": "Still life",
 	"link":"images/work_samples/drawings/6.jpg",
 	"image": "images/work_samples/drawings/6.jpg"
-},
-{
+},{
 	"organization": "Drawings",
 	"class":"drawings",
 	"title": "Study of Hand",
 	"link":"images/work_samples/drawings/7.jpg",
 	"image": "images/work_samples/drawings/7.jpg"
-},
-{
+},{
 	"organization": "Drawings",
 	"class":"drawings",
 	"title": "Paper Lillies",
