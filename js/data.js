14f98577f47a3ba31a015d2333affbe4c64c3db0
Cecilia Reyes <Cecilia.Reyes@propublica.org>
2015-11-25 13:44:43 -0500

    cleaned up some things, got rid of fun outlines, made sure nothing broke as roles added

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-data = 
+var data = 
 [{
 	"organization": "ProPublica",
 	"class": "pp",
@@ -15,105 +15,105 @@ data =
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "Faces of HS Football",
-	"role": "fdev",
+	"role": ["fdev"],
 	"link":"http://www.bostonglobe.com/2014/09/03/football/xXGFhl3CpPXRxl4uO2f0DN/story.html",
 	"image": "images/work_samples/globe/hsfootball1.png"
 },{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "MFA Masterpieces on Loan",
-	"role": "fdev design",
+	"role": ["fdev","design"],
 	"link":"http://www.bostonglobe.com/2014/08/02/mfa-masterpieces-loan/nKWiWAouJFiHC8GHOG39UI/story.html",
 	"image": "images/work_samples/globe/loanart.png"
 },{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "NFL Pick 'Em",
-	"role": "bdev",
+	"role": ["bdev"],
 	"link":"http://www.bostonglobe.com/2014/09/02/globe-nfl-pick/lrrDKN5JOcILOf4NAc95kN/story.html",
 	"image": "images/work_samples/globe/pickem.png"
 },{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "Ten Percenters",
-	"role": "fdev",
+	"role": ["fdev"],
 	"link":"http://www.bostonglobe.com/metro/2014/07/19/just-luck-some-lottery-winners-cash-hundreds-tickets/u9slYhTdzofrnTGMmGF0jN/story.html",
 	"image": "images/work_samples/globe/brudnicks.png"
 },{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "Mass. by Descendance",
-	"role": "fdev",
+	"role": ["fdev"],
 	"link":"http://www.bostonglobe.com/2014/07/14/what-team-did-massachusetts-root-for-during-world-cup-final/EypFKrpWFpLV4l7kvnvu8O/story.html",
 	"image": "images/work_samples/globe/worldcup.png"
 },{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "FIFA 2014: Will the U.S. advance if...",
-	"role": "bdev",
+	"role": ["bdev"],
 	"link":"http://www.bostonglobe.com/2014/06/23/soccer/pf21frmdANwSDtGvBVhWVK/story.html",
 	"image": "images/work_samples/globe/wc_calculator.png"
 },{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "Rising Tuition Costs in Boston Area",
-	"role": "fdev",
+	"role": ["fdev"],
 	"link":"http://www.bostonglobe.com/2014/06/22/compare-prices-colleges-and-universities/383o9UzIKNeHHm5yODNuQO/story.html",
 	"image": "images/work_samples/globe/colleges.png"
 },{
 	"organization": "Boston Globe",
 	"class": "bg",
 	"title": "Boston Mayoral Race",
-	"role": "fdev",
+	"role": ["fdev"],
 	"link":"http://www.bostonglobe.com/2014/06/05/poll-governor-election-candidates/rnLiEf8tFWjjaPFxXRvwRK/story.html",
 	"image": "images/work_samples/globe/favorability.png"
 },{
 	"organization": "New York Daily News",
 	"class": "nydn",
 	"title": "GEN Hackathon 2014",
-	"role": "fdev bdev design",
+	"role": ["fdev","bdev", "design"],
 	"link":"https://github.com/kcecireyes/NewsQs",
 	"image": "images/work_samples/nydn/newsqs.png"
 },{
 	"organization": "New York Daily News",
 	"class": "nydn",
 	"title": "What Does Autocomplete Say About...",
-	"role": "fdev design writer",
+	"role": ["fdev","design", "writer"],
 	"link":"http://www.nydailynews.com/new-york/map-reveals-google-autocomplete-new-york-city-article-1.1744115",
 	"image": "images/work_samples/nydn/google.png"
 },{
 	"organization": "New York Daily News",
 	"class": "nydn",
 	"title": "Sandy Anniversary",
-	"role": "fdev design",
+	"role": ["fdev", "design"],
 	"link":"http://www.nydailynews.com/new-york/hurricane-sandy/sandy-flood-zone-interactive-graphic-article-1.1499707",
 	"image": "images/work_samples/nydn/sandy.png"
 },{
 	"organization": "Columbia Spectator",
 	"class": "spec",
 	"title": "Faculty House Negotiations Stalled",
-	"role": "writer",
+	"role": ["writer"],
 	"link":"http://columbiaspectator.com/2013/01/16/students-faculty-house-employees-continue-negotiate-contracts-labor-administrators",
 	"image": "images/work_samples/spec/sws.jpg"
 },{
 	"organization": "Columbia Spectator",
 	"class": "spec",
 	"title": "TC Administrators Pocket $315,500 from Budget",
-	"role": "writer",
+	"role": ["writer"],
 	"link":"http://columbiaspectator.com/news/2013/05/10/teachers-college-faculty-vote-not-support-proposed-2013-14-budget",
 	"image": "images/work_samples/spec/tc.jpg"
 },{
 	"organization": "Columbia Spectator",
 	"class": "spec",
 	"title": "Columbia Nutella Craze",
-	"role": "writer",
+	"role": ["writer"],
 	"link":"http://columbiaspectator.com/2013/03/05/nutella-ferris-booth-costs-dining-5000-week-part-due-dining-hall-thievery",
 	"image": "images/work_samples/spec/nutella.jpg"
 },{
 	"organization": "Columbia Spectator",
 	"class": "spec",
 	"title": "Police Sting at Bar 1020",
-	"role": "writer",
+	"role": ["writer"],
 	"link":"http://columbiaspectator.com/2013/02/22/1020-fined-underage-drinking-after-police-sting-could-lose-liquor-license",
 	"image": "images/work_samples/spec/1020.jpg"
 },{
@@ -131,106 +131,114 @@ data =
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Sam and Justin's Wedding Invitation",
 	"link":"images/work_samples/drawings/wedding_front.png",
 	"image": "images/work_samples/drawings/wedding_front.png"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Postcard: Goodbye, Columbia",
 	"link":"images/work_samples/drawings/postcard.jpg",
 	"image": "images/work_samples/drawings/postcard.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Postcard: Tracey's Hair on Brighton",
 	"link":"images/work_samples/drawings/postcard_tracey.jpg",
 	"image": "images/work_samples/drawings/postcard_tracey.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Postcard: Adulthood for Alexandra",
 	"link":"images/work_samples/drawings/postcard_alex.jpg",
 	"image": "images/work_samples/drawings/postcard_alex.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Envelope for Andrew, collab",
 	"link":"images/work_samples/drawings/9.jpg",
 	"image": "images/work_samples/drawings/9.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Self-Loathing, watercolor",
 	"link":"images/work_samples/drawings/watercolor_1.jpg",
 	"image": "images/work_samples/drawings/watercolor_1.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Man Lying On His Side",
 	"link":"images/work_samples/drawings/2.jpg",
 	"image": "images/work_samples/drawings/2.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Older Man Lying Down",
 	"link":"images/work_samples/drawings/4.jpg",
 	"image": "images/work_samples/drawings/4.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Older Man, Detail",
 	"link":"images/work_samples/drawings/8.jpg",
 	"image": "images/work_samples/drawings/8.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Study of Legs",
 	"link":"images/work_samples/drawings/1.jpg",
 	"image": "images/work_samples/drawings/1.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Study, Hand & Foot",
 	"link":"images/work_samples/drawings/3.jpg",
 	"image": "images/work_samples/drawings/3.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Portrait of Sleeping Girl",
 	"link":"images/work_samples/drawings/5.jpg",
 	"image": "images/work_samples/drawings/5.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Still life on a Countertop",
 	"link":"images/work_samples/drawings/6.jpg",
 	"image": "images/work_samples/drawings/6.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Study of Hand",
 	"link":"images/work_samples/drawings/7.jpg",
 	"image": "images/work_samples/drawings/7.jpg"
 },{
 	"organization": "Drawings",
 	"class":"drawings",
-	"role": "design",
+	"role": ["ill"],
 	"title": "Paper Lillies",
 	"link":"images/work_samples/drawings/11.jpg",
 	"image": "images/work_samples/drawings/11.jpg"
-}]
+}];
+
+var map = {
+	"fdev": "Front end",
+	"bdev": "Back end",
+	"design": "Design",
+	"ill": "Illustrator",
+	"writer": "Writer",
+}
